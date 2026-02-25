8d032fc7101c098ca10fd20b1ea70bc55dfd7a6e
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-02-25 16:23:43 +0100

    Add check in std/lua.lua #73

--- plume-data/engine/std/lua.lua
+++ plume-data/engine/std/lua.lua
@@ -15,47 +15,58 @@ If not, see <https://www.gnu.org/licenses/>.
 
 return function (plume)
     
-   
     plume.stdLua = {
-        print = function(args, chunk)
-            local result = {}
-            for _, x in ipairs(args.table) do
-                table.insert(result, plume.repr(x))
+        print = {
+            method = function(args, chunk)
+                local result = {}
+                for _, x in ipairs(args.table) do
+                    table.insert(result, plume.repr(x))
+                end
+                print(table.unpack(result))
+                return true
             end
-            print(table.unpack(result))
-            return true
-        end,
-
-        --------------------------------------
-        -- WILL BE REMOVED IN 1.0 (#230, #414)
-        --------------------------------------
-        tonumber = plume.warning.deprecatedFunctionRuntime("1.0", "`tonumber` standard macro", "Use `Number` instead", {230, 414}, function(args, chunk)
-
-            local x = args.table[1]
-            if x == plume.obj.empty then
-                return false, "Cannot convert empty into number"
-            elseif type(x) == "number" then
-                return true, x
-            else
-               return false, string.format("Cannot convert %s into number", type(x))
-            end
-            return true, table.concat(result)
-        end),
-        --------------------------------------
+        },
+        
+        --------------------------------------  
+        -- WILL BE REMOVED IN 1.0 (#230, #414)  
+        --------------------------------------  
+        tonumber = {
+            method = plume.warning.deprecatedFunctionRuntime("1.0", "`tonumber` standard macro", "Use `Number` instead", {230, 414},function(args, chunk)
+                    local x = args.table[1]
+                    if x == plume.obj.empty then
+                        return false, "Cannot convert empty into number"
+                    elseif type(x) == "number" then
+                        return true, x
+                    else
+                       return false, string.format("Cannot convert %s into number", type(x))
+                    end
+                    return true, table.concat(result)
+                end
+            )
+        },
+        --------------------------------------  
 
         -- path
-        setPlumePath = function(args, runtime)
-            runtime.env.plume_path = args.table[1]
-            return true
-        end,
+        setPlumePath = {
+            checkArgs = {checkTypes={"string"}, args=1, signature="string path"},
+            method = function(args, runtime)
+                runtime.env.plume_path = args.table[1]
+                return true
+            end
+        },
 
-        addToPlumePath = function(args, runtime)
-            runtime.env.plume_path = (runtime.env.plume_path or "") .. ";" .. args.table[1]
-            return true
-        end,
+        addToPlumePath = {
+            checkArgs = {checkTypes={"string"}, args=1, signature="string path"},
+            method = function(args, runtime)
+                runtime.env.plume_path = (runtime.env.plume_path or "") .. ";" .. args.table[1]
+                return true
+            end
+        },
 
         -- io
-        write = function(args)
+        write = {
+            checkArgs = {checkTypes={"string"}, minArgs=1, maxArgs=math.huge, signature="string path, ...content"},
+            method = function(args)
             local filename = args.table[1]
             local content = table.concat(args.table, 2,  #args.table)
             local file = io.open(filename, "w")
@@ -65,9 +76,11 @@ return function (plume)
                 file:write(content)
             file:close()
             return true
-        end,
+        end },
 
-        read = function(args)
+        read = {
+            checkArgs = {checkTypes={"string"}, args=1, signature="string path"},
+            method = function(args)
             local filename = args.table[1]
             local file = io.open(filename)
                 if not file then
@@ -76,23 +89,29 @@ return function (plume)
                 local content = file:read("*a")
             file:close()
             return true, content
-        end,
+        end },
 
-        rawset = function(args)
-            local obj   = args.table[1]
-            local key   = args.table[2]
-            local value = args.table[3]
+        rawset = {
+            checkArgs = {checkTypes={"table", "string"}, args=3, signature="table t, string key, any value"},
+            method = function(args)
+                local obj   = args.table[1]
+                local key   = args.table[2]
+                local value = args.table[3]
 
-            if not obj.table[key] then
-                table.insert(obj.keys, key)
+                if not obj.table[key] then
+                    table.insert(obj.keys, key)
+                end
+                obj.table[key] = value
+                return true
             end
-            obj.table[key] = value
-            return true
-        end,
+        },
 
-        repr = function(args)
-            local obj = args.table[1]
-            return true, plume.repr(obj)
-        end
+        repr = {
+            checkArgs = {args=1, signature="any obj"},
+            method = function(args)
+                local obj = args.table[1]
+                return true, plume.repr(obj)
+            end
+        }
     }
 end
