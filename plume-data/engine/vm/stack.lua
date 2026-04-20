fdcc3037d60f68c6573767ef82bf5a0032fc7e0d
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-04-20 13:12:03 +0200

    Change license from GPLv3 to MIT

--- plume-data/engine/vm/stack.lua
+++ plume-data/engine/vm/stack.lua
@@ -1,16 +1,8 @@
---[[This file is part of Plume
+--[[
+This file is part of Plume🪶
 
-Plume🪶 is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, version 3 of the License.
-
-Plume🪶 is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with Plume🪶.
-If not, see <https://www.gnu.org/licenses/>.
+Copyright © Erwan Barbedor
+Licensed under the MIT License — see LICENSE for details.
 ]]
 
 ---------------------------------------  
