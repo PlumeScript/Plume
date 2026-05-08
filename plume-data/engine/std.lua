53c1f5f7681a050d5446eae9675d041be236e319
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-05-08 08:54:55 +0200

    Rewrite os.lua #752

---
+++ plume-data/engine/std.lua
@@ -0,0 +1,50 @@
+--[[
+This file is part of Plume🪶
+
+Copyright © Erwan Barbedor
+Licensed under the MIT License — see LICENSE for details.
+]]
+
+return function(plume)
+	plume.std.os = plume.obj.quickTable ({
+	    getEnv = plume.obj.luaMacro("getEnv", function (args)
+		------------
+		-- CHECKS --
+		------------
+		local __name      = "getEnv"
+		local __signature = "string name"
+		local __s, __e, name
+		__s, __e, name = plume.stdUnpackPositional(args, 1, 1, __name, __signature)
+		if __s then __s, __e = plume.stdCheckType(name, "string", "name", __name, __signature) end
+		if not __s then return false, __e end
+		------------
+		
+	        return true, os.getenv(name)
+	    end),
+	
+	    -- Very basic implementation
+	    execute = plume.obj.luaMacro("execute", function (args)
+		------------
+		-- CHECKS --
+		------------
+		local __name      = "execute"
+		local __signature = "string command"
+		local __s, __e, command
+		__s, __e, command = plume.stdUnpackPositional(args, 1, 1, __name, __signature)
+		if __s then __s, __e = plume.stdCheckType(command, "string", "command", __name, __signature) end
+		if not __s then return false, __e end
+		------------
+		
+	    	local success, result = pcall(function()
+	    	    local h = io.popen(command)
+	    		if not h then
+	    			return nil
+	    		end
+	    		local r = h:read("*a")
+	    		h:close()
+	    		return r
+	    	end)
+	        return success, result
+	    end)
+	})
+end
--- plume-data/engine/std/os.lua
+++ plume-data/engine/std/os.lua
@@ -1,44 +1,21 @@
---[[
-This file is part of Plume🪶
-
-Copyright © Erwan Barbedor
-Licensed under the MIT License — see LICENSE for details.
-]]
-
-return function (plume)
-	plume.std.os = plume.obj.table (0, 0)
-
-	plume.std.os.table.getEnv = {
-        checkArgs = {
-            checkTypes = {"string"},
-            signature = "string name",
-            named={self=true},
-            args=1,
-        },
-        method = function (x)
-            return true, os.getenv(x)
-        end
-    }
+plume.std.os = plume.obj.quickTable ({
+    getEnv = plume.obj.luaMacro("getEnv", function (args)
+        --!signature string name
+        return true, os.getenv(name)
+    end),
 
     -- Very basic implementation
-    plume.std.os.table.execute = {
-        checkArgs = {
-            checkTypes = {"string"},
-            signature = "string commande",
-            named={self=true},
-            args=1,
-        },
-        method = function (x)
-        	local success, result = pcall(function()
-			    local h = io.popen(x)
-				if not h then
-					return nil
-				end
-				local r = h:read("*a")
-				h:close()
-				return r
-			end)
-            return success, result
-        end
-    }
-end
+    execute = plume.obj.luaMacro("execute", function (args)
+        --!signature string command
+    	local success, result = pcall(function()
+    	    local h = io.popen(command)
+    		if not h then
+    			return nil
+    		end
+    		local r = h:read("*a")
+    		h:close()
+    		return r
+    	end)
+        return success, result
+    end)
+})
