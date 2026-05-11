24fadcddffa4cc6e59c0bc742232ae2deaccea83
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-05-11 21:34:15 +0200

    Table.removeKey should raise an error if key is missing #772

--- plume-data/engine/std.lua
+++ plume-data/engine/std.lua
@@ -1772,7 +1772,7 @@ return function(plume)
 	
 	        return pcall(table.concat, args, sep)
 	    end),
-	    removeKey = plume.obj.luaMacro("removeKey", function (args)
+	    removeKey = plume.obj.luaMacro("removeKey", function (args, runtime, _, ip)
 	        local __name      = "removeKey"
 	        local __signature = "`$removeKey(table t, any key)`"
 	        local __s, __e, self, t, key
@@ -1788,6 +1788,14 @@ return function(plume)
 	                index = k
 	                break
 	            end
+	        end
+	
+	        if index == 0 then
+	            plume.warning.runtimeWarning(
+	                string.format("The key `%s` doesn't exist. This will raise an error from edition Owl.", key),
+	                "Check if key exists before removing it.",
+	                runtime, ip, {614, 772}
+	            )
 	        end
 	
 	        t.table[key] = nil
