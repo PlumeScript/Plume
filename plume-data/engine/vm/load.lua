d270262b6a88afaccf4e9f7fe8c52029c3ad4571
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-05-11 19:46:26 +0200

    Revert "Replace error by _ERROR in vm (debugmode) #784"
    This reverts commit dac2bcd1df1dc62867f0f0adb3c8c387864a42b9.

--- plume-data/engine/vm/load.lua
+++ plume-data/engine/vm/load.lua
@@ -17,7 +17,7 @@ function LOAD_CONSTANT (vm, arg1, arg2)
 
     --! to-remove-begin
     if value == nil then
-        _ERROR (vm, "[VM] Try to load a nil value.")
+        error("[VM] Try to load a nil value.")
     end
     --! to-remove-end
 
