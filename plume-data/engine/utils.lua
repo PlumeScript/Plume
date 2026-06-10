b44221b0014b33958861a433d7f571d174099449
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-06-10 18:27:07 +0200

    Replace raise used as identifier warning with an error #735

--- plume-data/engine/utils.lua
+++ plume-data/engine/utils.lua
@@ -284,16 +284,10 @@ return function (plume)
 	end
 
 	function plume.checkIdentifier(node, identifier)
-		for kw in ('if then elseif else while for do macro let set const param use raw run ref with'):gmatch('%S+') do
+		for kw in ('if then elseif else while for do macro let set const param use raw run ref with raise'):gmatch('%S+') do
 			if identifier == kw then
 				plume.error.wrongIdentifier(node, identifier)
 			end
-		end
-
-		if identifier == "raise" then
-			plume.warning.throwWarning(
-				"Using `raise` as an identifier is deprecated, since it is now a keyword.\nThis will cause an error from edition 'Owl'.", nil, node, {185, 614, 742}
-			)
 		end
 	end
 
