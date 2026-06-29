5832936d5399746d7e4d18d4c675a9a7f4bcbd13
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-06-29 11:33:20 +0200

    Revert "Make type handler DEFAULT #943"
    This reverts commit 7821dde79641854c3b94546c390fa934c8c073b0.

--- plume-data/engine/ast.lua
+++ plume-data/engine/ast.lua
@@ -92,8 +92,9 @@ return function (plume)
 		end
 	end
 
-	local typeHandlerTable = {}
-	typeHandlerTable.DEFAULT = function(node, parentLastNode)
+
+	-- It needs to be completely rewritten; it's completely impossible to maintain at this point.
+	function plume.ast.markType(node, parentLastNode)
 		local waitOneValue = node.parent and (node.parent.name == "ELSE" or node.parent.name == "ELSEIF") and node.parent.type == "VALUE"
 
 		if node.parent and (
@@ -187,11 +188,6 @@ return function (plume)
 				node.type = "TEXT"
 			end
 		end
-	end
-
-	-- It needs to be completely rewritten; it's completely impossible to maintain at this point.
-	function plume.ast.markType(node, parentLastNode)
-		(typeHandlerTable[node.name] or typeHandlerTable.DEFAULT) (node, parentLastNode)
 
 		local primitiveType = primitiveTypes[node.name]
 		if primitiveType == "INHERIT" then
