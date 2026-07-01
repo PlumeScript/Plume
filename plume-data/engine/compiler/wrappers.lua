b4cc9ab0e5f00c18a77f2eb3e8c92e01f9a018c9
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-07-01 19:49:14 +0200

    Remove VALUE type. #951

--- plume-data/engine/compiler/wrappers.lua
+++ plume-data/engine/compiler/wrappers.lua
@@ -45,7 +45,20 @@ return function (plume, context)
         return function (node, label)
             local macro = context.getLast "macros"
 
-            if node.type == "TEXT" then
+            -- More or less a TEXT block with 1 element.
+            -- Don't use ACC_TEXT to prevent conversion to string
+            if node.isUnic then 
+                context.toggleConcatOff() 
+                f(node)
+                context.toggleConcatPop()
+
+                if #node.children == 1 and node.children[1].type == "TABLE" and infos then
+                    registerTableInfos(node, infos)
+                end
+                if label then  
+                    context.registerLabel(node, label)  
+                end  
+            elseif node.type == "TEXT" then
                 context.accBlockDeep = context.accBlockDeep + 1
                 context.registerOP(node, plume.ops.BEGIN_ACC, 0, 0)  
                 
@@ -67,70 +80,55 @@ return function (plume, context)
                 end  
                 
                 context.accBlockDeep = context.accBlockDeep - 1
-            else  
-                -- More or less a TEXT block with 1 element.
-                -- Don't use ACC_TEXT to prevent conversion to string
-                if node.type == "VALUE" or node.type == "VALUE_TABLE" or node.type == "VALUE_MACRO" then 
-                    context.toggleConcatOff() 
-                    f(node)
-                    context.toggleConcatPop()
+            -- Handled by block in most cases  
+            elseif node.type == "TABLE" then
+                local doc = context.collectComments(node)
 
-                    if #node.children == 1 and node.children[1].type == "TABLE" and infos then
-                        registerTableInfos(node, infos)
-                    end
-                    if label then  
-                        context.registerLabel(node, label)  
-                    end  
-                -- Handled by block in most cases  
-                elseif node.type == "TABLE" then
-                    local doc = context.collectComments(node)
+                context.accTableInit(node)
+                context.accBlockDeep = context.accBlockDeep + 1
 
-                    context.accTableInit(node)
-                    context.accBlockDeep = context.accBlockDeep + 1
+                if macro then
+                    table.insert(macro.blockToClose, plume.ops.CONCAT_TABLE)
+                end
 
-                    if macro then
-                        table.insert(macro.blockToClose, plume.ops.CONCAT_TABLE)
-                    end
+                context.toggleConcatOff()
+                f(node)
+                context.toggleConcatPop()
 
-                    context.toggleConcatOff()
-                    f(node)
-                    context.toggleConcatPop()
+                if macro then
+                    table.remove(macro.blockToClose)
+                end
 
-                    if macro then
-                        table.remove(macro.blockToClose)
-                    end
+                context.registerOP(nil, plume.ops.CONCAT_TABLE, 0, 0)
+                if doc and #doc>0 then
+                    infos = infos or {}
+                    table.insert(infos, {"doc", doc})
+                end
+                if infos then
+                    registerTableInfos(node, infos)
+                end
+                
+                if label then  
+                    context.registerLabel(node, label)  
+                end
+                context.accBlockDeep = context.accBlockDeep - 1
+            -- Exactly same behavior as BEGIN_ACC (nothing) ACC_TEXT
+            elseif node.type == "EMPTY" then
+                context.toggleConcatOff()
+                f(node)
+                context.toggleConcatPop()
 
-                    context.registerOP(nil, plume.ops.CONCAT_TABLE, 0, 0)
-                    if doc and #doc>0 then
-                        infos = infos or {}
-                        table.insert(infos, {"doc", doc})
+                if label then  
+                    context.registerLabel(node, label)  
+                end
+                local parentName = node.parent and node.parent.name
+                if parentName ~= "WITH" and parentName ~= "DO" then -- not the cleanest workaround
+                    if context.checkIfCanConcat() then
+                        context.registerOP(nil, plume.ops.LOAD_CONSTANT, 0, context.registerConstant(""))
+                    else
+                        context.registerOP(nil, plume.ops.LOAD_EMPTY, 0, 0)
                     end
-                    if infos then
-                        registerTableInfos(node, infos)
-                    end
-                    
-                    if label then  
-                        context.registerLabel(node, label)  
-                    end
-                    context.accBlockDeep = context.accBlockDeep - 1
-                -- Exactly same behavior as BEGIN_ACC (nothing) ACC_TEXT
-                elseif node.type == "EMPTY" then
-                    context.toggleConcatOff()
-                    f(node)
-                    context.toggleConcatPop()
-
-                    if label then  
-                        context.registerLabel(node, label)  
-                    end
-                    local parentName = node.parent and node.parent.name
-                    if parentName ~= "WITH" and parentName ~= "DO" then -- not the cleanest workaround
-                        if context.checkIfCanConcat() then
-                            context.registerOP(nil, plume.ops.LOAD_CONSTANT, 0, context.registerConstant(""))
-                        else
-                            context.registerOP(nil, plume.ops.LOAD_EMPTY, 0, 0)
-                        end
-                    end
-                end  
+                end
             end
         end          
     end  
