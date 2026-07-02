4caec71d9ce99c9be0048cc6bb32f9324b4bd961
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-07-02 13:05:13 +0200

    [BUG] Concat error in a certain nested case #959

--- plume-data/engine/compiler/handlers/branch.lua
+++ plume-data/engine/compiler/handlers/branch.lua
@@ -67,9 +67,7 @@ return function (plume, context, nodeHandlerTable)
 				context.scope()(body)
 			end
 			if specialValueMode and body.type == "EMPTY" then
-				if not context.checkIfCanConcat() then
-					context.registerOP(node, plume.ops.LOAD_EMPTY)
-				end
+				context.registerOP(node, plume.ops.LOAD_EMPTY)
 			end
 
 			context.registerGoto(node, "branch_"..finalBranch.."_"..uid)
