72359d7f87f2afbb98973468b31dfea9255d7b68
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-07-24 08:26:59 +0200

    debug mode: enhance long object representation #1041

--- plume-data/engine/debug/run.lua
+++ plume-data/engine/debug/run.lua
@@ -96,6 +96,22 @@ return function (plume)
 		return table.concat(result)
 	end
 
+	local function reprStackElement(obj)
+		local repr = plume.repr(obj)
+		local tooltip = ""
+		if #repr > 20 then
+			tooltip = string.format("<div class='stack-element--tooltip'>%s</div>", escape(repr))
+			repr = repr:sub(1, 17) .. "..."
+		end
+
+		return string.format(
+				"<div class='stack-element'>%s%s</div>",
+				escape(repr), tooltip
+			)
+
+		
+	end
+
 	local function renderExecution(log)
 		local result = {}
 		for i, vm in ipairs(log) do
@@ -119,7 +135,7 @@ return function (plume)
 									table.insert(result, string.format("<div class='frame-separator'></div>"))
 								end
 							end
-							table.insert(result, string.format("<div class='stack-element'>%s</div>", escape(plume.repr(vm.mainStack[j]))))
+							table.insert(result, reprStackElement(vm.mainStack[j]))
 						end
 						table.insert(result, "</div>")
 					table.insert(result, "</div>")
@@ -132,10 +148,7 @@ return function (plume)
 									table.insert(result, string.format("<div class='frame-separator'></div>"))
 								end
 							end
-							table.insert(result, string.format(
-								"<div class='stack-element'>%s</div>",
-								escape(plume.repr(vm.variableStack[j]))
-							))
+							table.insert(result,reprStackElement(vm.variableStack[j]))
 						end
 						table.insert(result, "</div>")
 					table.insert(result, "</div>")
