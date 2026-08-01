e31fc9cbb143375af56f1e7baecb199429e60840
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-08-01 20:53:03 +0200

    Update version number

--- plume-data/engine/init.lua
+++ plume-data/engine/init.lua
@@ -1,5 +1,5 @@
 --[[
-Plume🪶 b60 (Owl Edition)
+Plume🪶 b61 (Owl Edition)
 
 Copyright © 2024-2026 Erwan Barbedor
 
@@ -30,7 +30,7 @@ else
 end
 
 local plume = {}
-plume.VERSION = "b60 (Owl Edition)"
+plume.VERSION = "b61 (Owl Edition)"
 
 require 'plume-data/engine/error/core'    (plume)
 require 'plume-data/engine/warning'       (plume)
